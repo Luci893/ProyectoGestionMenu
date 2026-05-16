bdaed735d1394722c60c5b1da9788bddce796df7
LuciT893 <lucitorres9909@gmail.com>
2026-05-16 17:34:46 -0300

    Initial commit

---
+++ src/main/java/com/example/proyectomenu/ProyectoMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectomenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoMenuApplication.class, args);
+    }
+
+}
